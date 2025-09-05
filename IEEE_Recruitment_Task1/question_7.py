df83642201e119b8a1a652edc6b74676a33d925d
rolling-thunder18 <nissanmohanty18@gmail.com>
2025-09-05 14:09:30 +0530

    Update question_7.py

--- IEEE_Recruitment_Task1/question_7.py
+++ IEEE_Recruitment_Task1/question_7.py
@@ -1,7 +1,4 @@
-import numpy as np
-arr1=np.random.randint(1,10,(5,1))
-arr2=np.random.randint(1,10,(5,1))
-
-
-intersection=list(set(arr1.flatten()) & set(arr2.flatten()))
+arr1=[1,3,3,4,8,9,7]
+arr2=[5,7,2,9,4,3,6]
+intersection=list(set(arr1) & set(arr2))
 print(intersection)
