be969f738f9b73ed96ee8d049988d841d36eb0df
rolling-thunder18 <nissanmohanty18@gmail.com>
2025-09-05 13:50:39 +0530

    Update question_1.py

--- IEEE_Recruitment_Task1/question_1.py
+++ IEEE_Recruitment_Task1/question_1.py
@@ -1,10 +1,13 @@
 
-while True:
+while True: #starts a loop that continues till user inputs 0
     n=int(input("Enter the number of rows to display(enter 0 to exit): "))
+    #adds conditions
     if n==0:
-        break
+        break #a=breaks the code here aka ends loop with input 0
     for i in range(1,n+1):
-        while i<=n:
+        while i<=n: #loops till n
             print("* "*i)
             i+=1
         break
+
+
