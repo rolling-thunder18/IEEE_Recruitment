d2671b73e4ec59e7a9039759c23378662a6399a5
rolling-thunder18 <nissanmohanty18@gmail.com>
2025-09-05 13:57:07 +0530

    Update question_2.py

--- IEEE_Recruitment_Task1/question_2.py
+++ IEEE_Recruitment_Task1/question_2.py
@@ -1,8 +1,9 @@
 x=input("enter the paragraph: ")
-y=x.split()
+y=x.split() #splits the strings into individual words or list
 
-pld=[word for word in y if word==word[::-1] and len(word)>1]
+pld=[word for word in y if word==word[::-1] and len(word)>1] #ignores single letter words 
 if pld:
     print("the palindromes are: ",pld)
 else:
     print(0)
+
